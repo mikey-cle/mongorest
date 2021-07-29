ff3c8c44f5a03fa8504531225eead8ca96888299
Mikey Cleworth <81911172+mikey-cle@users.noreply.github.com>
2021-07-29 16:18:34 +0100

    added CRUD for users

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -19,21 +19,39 @@ exports.findUser = async (req, res) => {
   } catch (error) {
     res.status(500).send(error);
   }
-  exports.deleteUser = async (req, res) => {
-    try {
-      const user = req.params.username;
-      const userToDelete = await User.findOneAndDelete({ username: user }, (err, res)=>
-      {
-        if(err){
-          res.status(500).send(err)
-        }
-        else{
-          res.status(200).send(err)
-        }
-      });
-      res.status(200).send({ username: userToDelete });
-    } catch (error) {
-      res.status(500).send(error);
-    }
+};
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = req.params.username;
+    const deleteUser = await User.findOneAndRemove({ username: user });
+    res.status(200).send({
+      username: deleteUser,
+      message: `${user} has been removed from the database.`,
+    });
+  } catch (error) {
+    res.status(500).send(error);
   }
-}
+};
+exports.updateEmail = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const newEmail = req.body.newEmail;
+    await User.findOneAndUpdate({ username: username }, { email: newEmail });
+    res.status(200).send({ username, newEmail });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+exports.updatePass = async (req, res) => {
+  try {
+    const username = req.body.username;
+    const newPass = req.body.newPass;
+    const changed = await User.findOneAndUpdate(
+      { username: username },
+      { password: newPass }
+    );
+    res.status(200).send({ username, newPass });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const userRouter = Router();
-const {createUser,updateUser,findUser,deleteUser} = require("./users.controllers");
+const {createUser,findUser,deleteUser, updateEmail, updatePass} = require("./users.controllers");
 userRouter.post("/users", createUser);
 userRouter.get("/users/:username", findUser);
-// userRouter.put("/users/:username", updateUser); // write func
+userRouter.put("/users/:username/", updateEmail); 
 userRouter.delete("/users/:username", deleteUser);
+userRouter.put("users/updatePass", updatePass)
 
 module.exports = userRouter;
